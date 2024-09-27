50c56176d007e4faafd8293eddc159f43c5a3a7a
Graham <grahamjonesgs@gmail.com>
2024-09-27 10:41:11 +0200

    Fixed titles

--- graph.php
+++ graph.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'vars.php';
+include 'vars.php'; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
 $dataType=$_GET["dataType"];
 $days=$_GET["days"];
 
-$sql = "SELECT * FROM (SELECT  * FROM rec_data where type = $dataType and dt >= NOW() - INTERVAL $days DAY order by rand() limit 50000) alias order by room_id, dt";
+$sql = "SELECT * FROM (SELECT  * FROM rec_data where type = $dataType and dt >= NOW() - INTERVAL $days DAY order by rand() limit 5000) alias order by room_id, dt";
 //select * from (select * from rec_data order by RAND() limit 10) alias order by dt;
 
 $result = $conn->query($sql);
